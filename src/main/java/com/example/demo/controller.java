9a983bfbb7927a1d8da830943b71c04b49dcc1b4
Totallynotshid <Totallynotshid@gmail.com>
2025-02-20 12:25:45 -0500

    Modify Greeting Message
    Adds new greeting

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalTime;
 
 @RestController
 public class controller {
     @GetMapping("/hello")
     public String hello() {
-        return "Welcome To COMP367";
+        String name = "Rashid Leroy Ang"; // Replace with your name
+        LocalTime now = LocalTime.now();
+        if (now.isBefore(LocalTime.NOON)) {
+            return "Good morning, " + name + ", Welcome to COMP367";
+        } else {
+            return "Good afternoon, " + name + ", Welcome to COMP367";
+        }
     }
 }
